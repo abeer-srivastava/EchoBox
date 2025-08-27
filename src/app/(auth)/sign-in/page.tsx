5865c32cb9e5ebf4cd149365f65f0e1763918785
abeer-srivastava <abeersrivastava16@gmail.com>
2025-08-27 19:04:03 +0530

    fixing the signin page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -51,7 +51,7 @@ function Page() {
     <div className="flex justify-center items-center min-h-screen bg-gradient-to-b from-emerald-50 via-white to-lime-50">
       <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
         <div className="text-center">
-          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+          <h1 className="text-4xl font-black tracking-tight lg:text-5xl mb-6 ">
             Welcome Back to Mystery Message 
           </h1>
           <p className="mb-4">Sign in to continue your secret conversations</p>
